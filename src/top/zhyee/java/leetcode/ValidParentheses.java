ea7674a3943e1b14e927725a002208acb8eabefd
yanyzy <1181862765@qq.com>
2018-04-17 11:35:57 +0800

    leetcode NumberOf1Bits

--- src/top/zhyee/java/leetcode/ValidParentheses.java
+++ src/top/zhyee/java/leetcode/ValidParentheses.java
@@ -4,44 +4,48 @@ import java.util.Stack;
 
 public class ValidParentheses {
     public boolean isValid(String s) {
-        Boolean b = false;
-        Stack stack = new Stack();
+        Stack<Character> stack = new Stack<>();
         if (s.length() % 2 != 0) {
-            return b;
+            return false;
         }
-        for(int i=0;i<s.length();i++){
-            if(s.charAt(i)=='{'||s.charAt(i)=='('||s.charAt(i)=='['){
-                stack.push(s.charAt(i));
+        for (int i = 0; i < s.length(); i++) {
+            char nowChar = s.charAt(i);
+
+            if (isLeftCouple(nowChar)) {
+                stack.push(nowChar);
+                continue;
             }
-            else if(s.charAt(i)=='}'||s.charAt(i)==')'||s.charAt(i)==']'){
-                if(stack.empty()){
-                    return b;
-                }
-                char c = (char) stack.peek();
-                if(c=='{'){
-                    if(s.charAt(i)!='}'){
-                        return b;
-                    }
-                }
-                else if(c=='['){
-                    if(s.charAt(i)!=']'){
-                        return b;
-                    }
-                }
-                else if(c=='('){
-                    if(s.charAt(i)!=')'){
-                        return b;
-                    }
-                }
-                stack.pop();
+
+            if (stack.empty()) {
+                return false;
             }
+
+            char preChar = stack.peek();
+
+            if (!isCouple(preChar, nowChar)) {
+                return false;
+            }
+            stack.pop();
         }
-        if(stack.empty()){
-            b = true;
-        }
-        return b;
+        return stack.empty();
     }
 
+    private static boolean isLeftCouple(char a) {
+        return a == '(' || a == '{' || a == '[';
+    }
+
+    private static boolean isCouple(char a, char b) {
+        switch (a) {
+            case '(':
+                return b == ')';
+            case '[':
+                return b == ']';
+            case '{':
+                return b == '}';
+            default:
+                return false;
+        }
+    }
 
 
     public static void main(String[] args) {
