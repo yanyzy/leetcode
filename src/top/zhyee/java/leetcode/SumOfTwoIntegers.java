ba8b7ecc577f212fb9b0655e2b5b850dd4b306ac
yanyzy <1181862765@qq.com>
2019-04-04 10:38:40 +0800

    commit

---
+++ src/top/zhyee/java/leetcode/SumOfTwoIntegers.java
@@ -0,0 +1,40 @@
+package top.zhyee.java.leetcode;
+
+/**
+ * @author zhyee
+ * @date 2019/4/4 上午10:35
+ */
+
+
+/**
+ * 371. 两整数之和
+ * <p>
+ * <p>
+ * 不使用运算符 + 和 - ​​​​​​​，计算两整数 ​​​​​​​a 、b ​​​​​​​之和。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: a = 1, b = 2
+ * 输出: 3
+ * 示例 2:
+ * <p>
+ * 输入: a = -2, b = 3
+ * 输出: 1
+ */
+public class SumOfTwoIntegers {
+
+    public int getSum(int a, int b) {
+
+        if (b > 0) {
+            for (int i = 0; i < b; i++) {
+                a++;
+            }
+        } else if (b < 0) {
+            for (int i = b; i < 0; i++) {
+                a--;
+            }
+        }
+
+        return a;
+    }
+}
