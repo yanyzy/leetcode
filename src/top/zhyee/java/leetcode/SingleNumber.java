4b16ed5ecf66def739f971368d49cb8aaf3cec58
yanyzy <1181862765@qq.com>
2019-04-10 17:05:15 +0800

    commit

--- src/top/zhyee/java/leetcode/SingleNumber.java
+++ src/top/zhyee/java/leetcode/SingleNumber.java
@@ -25,6 +25,13 @@ package top.zhyee.java.leetcode;
  */
 public class SingleNumber {
 
+    /**
+     * 异或，相同为0，不同为1
+     * 异或一个数两次为原来的数
+     *
+     * @param nums
+     * @return
+     */
     public int singleNumber(int[] nums) {
 
         int result = nums[0];
