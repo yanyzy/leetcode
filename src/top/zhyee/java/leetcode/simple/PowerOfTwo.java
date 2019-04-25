bcdf98ca849a29aaa224ed18a4e72620cd5ef154
yanyzy <1181862765@qq.com>
2019-04-25 16:14:14 +0800

    commit

---
+++ src/top/zhyee/java/leetcode/simple/PowerOfTwo.java
@@ -0,0 +1,35 @@
+package top.zhyee.java.leetcode.simple;
+
+/**
+ * @author zhyee
+ * @date 2019/4/25 下午4:07
+ */
+
+/**
+ * 231. 2的幂
+ *
+ * 给定一个整数，编写一个函数来判断它是否是 2 的幂次方。
+ *
+ * 示例 1:
+ *
+ * 输入: 1
+ * 输出: true
+ * 解释: 20 = 1
+ * 示例 2:
+ *
+ * 输入: 16
+ * 输出: true
+ * 解释: 24 = 16
+ * 示例 3:
+ *
+ * 输入: 218
+ * 输出: false
+ */
+public class PowerOfTwo {
+    public boolean isPowerOfTwo(int n) {
+        if (n <= 0) {
+            return false;
+        }
+        return (n & (n - 1)) == 0;
+    }
+}
