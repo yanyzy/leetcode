7e00184c0852e9b61a820ddde87c07f0d08e391e
yanyzy <1181862765@qq.com>
2019-05-21 14:29:23 +0800

    perf(Add Valid Anagram): :sparkles:

---
+++ src/top/zhyee/java/leetcode/simple/ValidAnagram.java
@@ -0,0 +1,58 @@
+package top.zhyee.java.leetcode.simple;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author zhyee
+ * @date 2019/5/21 下午2:18
+ */
+
+
+/**
+ * 242. 有效的字母异位词
+ * <p>
+ * 给定两个字符串 s 和 t ，编写一个函数来判断 t 是否是 s 的一个字母异位词。
+ * <p>
+ * 示例 1:
+ * <p>
+ * 输入: s = "anagram", t = "nagaram"
+ * 输出: true
+ * 示例 2:
+ * <p>
+ * 输入: s = "rat", t = "car"
+ * 输出: false
+ * 说明:
+ * 你可以假设字符串只包含小写字母。
+ * <p>
+ * 进阶:
+ * 如果输入字符串包含 unicode 字符怎么办？你能否调整你的解法来应对这种情况？
+ */
+public class ValidAnagram {
+    public boolean isAnagram(String s, String t) {
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (map.containsKey(s.charAt(i))) {
+                map.put(s.charAt(i), map.get(s.charAt(i)) + 1);
+            } else {
+                map.put(s.charAt(i), 1);
+            }
+        }
+
+        for (int i = 0; i < t.length(); i++) {
+            if (map.containsKey(t.charAt(i))) {
+                map.put(t.charAt(i), map.get(t.charAt(i)) - 1);
+                if (map.get(t.charAt(i)) == 0) {
+                    map.remove(t.charAt(i));
+                }
+            } else {
+                return false;
+            }
+        }
+
+        if (map.isEmpty()) {
+            return true;
+        }
+        return false;
+    }
+}
