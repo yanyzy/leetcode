52ced5a317417543e0a1b60cc4f5f0f4199eac1d
yanyzy <1181862765@qq.com>
2020-04-20 17:58:53 +0800

    add LinkedListCycleLCCI

--- src/top/zhyee/java/leetcode/medium/LCOF.java
+++ src/top/zhyee/java/leetcode/medium/LCOF.java
@@ -3,6 +3,31 @@ package top.zhyee.java.leetcode.medium;
 import java.util.HashMap;
 import java.util.Map;
 
+
+/**
+ * 面试题35. 复杂链表的复制
+ *
+ * 请实现 copyRandomList 函数，复制一个复杂链表。在复杂链表中，每个节点除了有一个 next 指针指向下一个节点，还有一个 random 指针指向链表中的任意节点或者 null。
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ *
+ *
+ * 输入：head = [[7,null],[13,0],[11,4],[10,2],[1,0]]
+ * 输出：[[7,null],[13,0],[11,4],[10,2],[1,0]]
+ * 示例 2：
+ *
+ *
+ *
+ * 输入：head = [[1,1],[2,1]]
+ * 输出：[[1,1],[2,1]]
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/fu-za-lian-biao-de-fu-zhi-lcof
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
 public class LCOF {
 
     //用map储存复制节点
---
+++ src/top/zhyee/java/leetcode/medium/LinkedListCycleLCCI.java
@@ -0,0 +1,72 @@
+package top.zhyee.java.leetcode.medium;
+
+import top.zhyee.java.leetcode.ListNode;
+
+
+/**
+ * 面试题 02.08. 环路检测
+ * <p>
+ * 给定一个有环链表，实现一个算法返回环路的开头节点。
+ * 有环链表的定义：在链表中某个节点的next元素指向在它前面出现过的节点，则表明该链表存在环路。
+ * <p>
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：head = [3,2,0,-4], pos = 1
+ * 输出：tail connects to node index 1
+ * 解释：链表中有一个环，其尾部连接到第二个节点。
+ * <p>
+ * 示例 2：
+ * <p>
+ * 输入：head = [1,2], pos = 0
+ * 输出：tail connects to node index 0
+ * 解释：链表中有一个环，其尾部连接到第一个节点。
+ * <p>
+ * 示例 3：
+ * <p>
+ * 输入：head = [1], pos = -1
+ * 输出：no cycle
+ * 解释：链表中没有环。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/linked-list-cycle-lcci
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class LinkedListCycleLCCI {
+    
+    public ListNode detectCycle(ListNode head) {
+        if (head == null || head.next == null) {
+            return null;
+        }
+        ListNode fast = head;
+        ListNode slow = head;
+        
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (fast == slow) {
+                fast = head;
+                while (fast != slow) {
+                    fast = fast.next;
+                    slow = slow.next;
+                }
+                return slow;
+            }
+        }
+        
+        return null;
+    }
+    
+    public static void main(String[] args) {
+        ListNode node = new ListNode(3);
+        ListNode node1 = new ListNode(2);
+        ListNode node2 = new ListNode(1);
+        ListNode node3 = new ListNode(4);
+        node.next = node1;
+        node1.next = node2;
+        node2.next = node3;
+        node3.next = node;
+        
+        System.out.println(new LinkedListCycleLCCI().detectCycle(node));
+    }
+}
