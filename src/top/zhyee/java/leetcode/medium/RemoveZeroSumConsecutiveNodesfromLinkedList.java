aec984b1d2048c6eb631d943dc83c88ff2c2ed45
yanyzy <1181862765@qq.com>
2020-04-22 11:18:48 +0800

    add RemoveZeroSumConsecutiveNodesfromLinkedList

---
+++ src/top/zhyee/java/leetcode/medium/RemoveZeroSumConsecutiveNodesfromLinkedList.java
@@ -0,0 +1,66 @@
+package top.zhyee.java.leetcode.medium;
+
+import top.zhyee.java.leetcode.ListNode;
+
+
+/**
+ * 1171. 从链表中删去总和值为零的连续节点
+ * <p>
+ * 给你一个链表的头节点 head，请你编写代码，反复删去链表中由 总和 值为 0 的连续节点组成的序列，直到不存在这样的序列为止。
+ * <p>
+ * 删除完毕后，请你返回最终结果链表的头节点。
+ * <p>
+ *  
+ * <p>
+ * 你可以返回任何满足题目要求的答案。
+ * <p>
+ * （注意，下面示例中的所有序列，都是对 ListNode 对象序列化的表示。）
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：head = [1,2,-3,3,1]
+ * 输出：[3,1]
+ * 提示：答案 [1,2,1] 也是正确的。
+ * 示例 2：
+ * <p>
+ * 输入：head = [1,2,3,-3,4]
+ * 输出：[1,2,4]
+ * 示例 3：
+ * <p>
+ * 输入：head = [1,2,3,-3,-2]
+ * 输出：[1]
+ *  
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/remove-zero-sum-consecutive-nodes-from-linked-list
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class RemoveZeroSumConsecutiveNodesfromLinkedList {
+    
+    public ListNode removeZeroSumSublists(ListNode head) {
+        ListNode node = head;
+        int count = 0;
+        while (node != null) {
+            count = count + node.val;
+            if (count == 0) {
+                head = node.next;
+            }
+            node = node.next;
+        }
+        if (head == null) {
+            return head;
+        }
+        ListNode temp = removeZeroSumSublists(head.next);
+        head.next = temp;
+        return head;
+    }
+    
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(-3);
+        head.next.next.next.next = new ListNode(-2);
+        System.out.println(new RemoveZeroSumConsecutiveNodesfromLinkedList().removeZeroSumSublists(head));
+    }
+}
