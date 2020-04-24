1adedcd21acd8fb133302a4c37678e0687d089ae
yanyzy <1181862765@qq.com>
2020-04-24 15:50:16 +0800

    add ThreeSum

---
+++ src/top/zhyee/java/leetcode/medium/ThreeSum.java
@@ -0,0 +1,59 @@
+package top.zhyee.java.leetcode.medium;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * 15. 三数之和
+ *
+ * 给你一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0 ？请你找出所有满足条件且不重复的三元组。
+ *
+ * 注意：答案中不可以包含重复的三元组。
+ *
+ *  
+ *
+ * 示例：
+ *
+ * 给定数组 nums = [-1, 0, 1, 2, -1, -4]，
+ *
+ * 满足要求的三元组集合为：
+ * [
+ *   [-1, 0, 1],
+ *   [-1, -1, 2]
+ * ]
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/3sum
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class ThreeSum {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> lists = new ArrayList<>();
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; i++) {
+            int start = i + 1;
+            int end = nums.length - 1;
+            while (start < end) {
+                int count = nums[i] + nums[start] + nums[end];
+                if (count == 0) {
+                    List<Integer> list = new ArrayList<>();
+                    list.add(nums[i]);
+                    list.add(nums[start]);
+                    list.add(nums[end]);
+                    lists.add(list);
+                    start++;
+                    end--;
+                } else if (count < 0) {
+                    start++;
+                } else {
+                    end--;
+                }
+            }
+        }
+        return lists.stream()
+                .distinct()
+                .collect(Collectors.toList());
+    }
+}
