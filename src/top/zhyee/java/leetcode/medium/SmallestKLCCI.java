205fe48f21f8cb63a5c7a24abe59cada2389dc9f
yanyzy <1181862765@qq.com>
2020-04-28 15:15:38 +0800

    add SmallestKLCCI

---
+++ src/top/zhyee/java/leetcode/medium/SmallestKLCCI.java
@@ -0,0 +1,42 @@
+package top.zhyee.java.leetcode.medium;
+
+
+/**
+ * 面试题 17.14. 最小K个数
+ * <p>
+ * 设计一个算法，找出数组中最小的k个数。以任意顺序返回这k个数均可。
+ * <p>
+ * 示例：
+ * <p>
+ * 输入： arr = [1,3,5,7,2,4,6,8], k = 4
+ * 输出： [1,2,3,4]
+ * 提示：
+ * <p>
+ * 0 <= len(arr) <= 100000
+ * 0 <= k <= min(100000, len(arr))
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/smallest-k-lcci
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class SmallestKLCCI {
+    //todo 冒泡排序超出时间限制
+    public int[] smallestK(int[] arr, int k) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j + 1];
+                    arr[j + 1] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+        int[] newArr = new int[k];
+        k--;
+        while (k >= 0) {
+            newArr[k] = arr[k];
+            k--;
+        }
+        return newArr;
+    }
+}
