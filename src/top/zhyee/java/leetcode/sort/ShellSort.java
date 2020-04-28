cb8f95ca776036ac1c91a654c8c15bcbb718501f
yanyzy <1181862765@qq.com>
2020-04-28 17:14:07 +0800

    fix ShellSort

--- src/top/zhyee/java/leetcode/sort/ShellSort.java
+++ src/top/zhyee/java/leetcode/sort/ShellSort.java
@@ -1,5 +1,11 @@
 package top.zhyee.java.leetcode.sort;
 
+
+/**
+ * 希尔排序
+ * <p>
+ * 按步进将数组分为若干小组，对这些小组分别进行插入排序，再修改步进，直到步进为0
+ */
 public class ShellSort {
     public void sort(int[] arr) {
         for (int i = arr.length / 2; i > 0; i = i / 2) {
--- src/top/zhyee/java/leetcode/sort/SortTest.java
+++ src/top/zhyee/java/leetcode/sort/SortTest.java
@@ -5,8 +5,11 @@ import java.util.Arrays;
 public class SortTest {
     public static void main(String[] args) {
         int arr_1[] = new int[]{1, 4, 2, 6, 24, 66, 88, 34, 233, 97, 5, 22};
+        int arr_2[] = new int[]{1, 4, 2, 6, 24, 66, 88, 34, 233, 97, 5};
         //todo fix insertion sort/shell sort
         new ShellSort().sort(arr_1);
+        new ShellSort().sort(arr_2);
         System.out.println(Arrays.toString(arr_1));
+        System.out.println(Arrays.toString(arr_2));
     }
 }
