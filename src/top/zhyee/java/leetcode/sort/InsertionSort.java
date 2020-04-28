04320644bb4082ee4c7f9b2c5930b5756a564f6f
yanyzy <1181862765@qq.com>
2020-04-28 15:56:08 +0800

    add
    InsertionSort

---
+++ src/top/zhyee/java/leetcode/sort/InsertionSort.java
@@ -0,0 +1,21 @@
+package top.zhyee.java.leetcode.sort;
+
+
+/**
+ * 插入排序
+ * <p>
+ * 遍历每一个元素，当发现其小于前面的元素，则不断将前面的元素向后移动
+ */
+public class InsertionSort {
+    public void sort(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            int index = i - 1;
+            int current = arr[i];
+            while (index >= 0 && arr[index + 1] < arr[index]) {
+                arr[index + 1] = arr[index];
+                index--;
+            }
+            arr[index + 1] = current;
+        }
+    }
+}
