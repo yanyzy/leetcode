3f0b708fd3c93d343848ec4c781a92f11d0441a2
yanyzy <1181862765@qq.com>
2020-04-30 14:45:34 +0800

    commit

---
+++ src/top/zhyee/java/leetcode/sort/BucketSort.java
@@ -0,0 +1,16 @@
+package top.zhyee.java.leetcode.sort;
+
+//todo 桶排序
+public class BucketSort {
+    public int[] sort(int[] arr) {
+        //先找出数组的范围
+        int max = arr[0];
+        for (int value : arr) {
+            max = Math.max(max, value);
+        }
+        
+        //初始化桶
+        
+        return null;
+    }
+}
