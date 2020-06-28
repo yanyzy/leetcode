6b11e76ff178b7a591af8b3fe7f5b4478c1b4384
zhyee <1181862765@qq.com>
2020-06-28 15:22:01 +0800

    commit

---
+++ src/top/zhyee/java/leetcode/dp/FibonacciNumber.java
@@ -0,0 +1,18 @@
+package top.zhyee.java.leetcode.dp;
+
+public class FibonacciNumber {
+    
+    public int fib(int N) {
+        if(N<2){
+            return N;
+        }
+        int[] dp = new int[N+1];
+        dp[0] = 0;
+        dp[1] = 1;
+        
+        for (int i = 2; i < dp.length; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[N];
+    }
+}
---
+++ src/top/zhyee/java/leetcode/dp/NthTribonacciNumber.java
@@ -0,0 +1,18 @@
+package top.zhyee.java.leetcode.dp;
+
+public class NthTribonacciNumber {
+    
+    public int tribonacci(int n) {
+        if (n < 2) {
+            return n;
+        }
+        int[] dp = new int[n + 1];
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 1;
+        for (int i = 3; i < dp.length; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+        }
+        return dp[n];
+    }
+}
