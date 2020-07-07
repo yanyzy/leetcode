024344af3bad313251e14e83784d901399249a4c
zhyee <1181862765@qq.com>
2020-07-07 15:01:17 +0800

    commit

---
+++ src/top/zhyee/java/leetcode/simple/CompressStringLCCI.java
@@ -0,0 +1,37 @@
+package top.zhyee.java.leetcode.simple;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CompressStringLCCI {
+    
+    public String compressString(String S) {
+        if (S.length() == 0) {
+            return S;
+        }
+        StringBuilder stringBuilder = new StringBuilder();
+        Character c = S.charAt(0);
+        int count = 1;
+        for (int i = 1; i < S.length(); i++) {
+            if (S.charAt(i) == c) {
+                count++;
+            } else {
+                stringBuilder.append(c);
+                stringBuilder.append(count);
+                c = S.charAt(i);
+                count = 1;
+            }
+        }
+        stringBuilder.append(c);
+        stringBuilder.append(count);
+        String s = stringBuilder.toString();
+        if (s.length() >= S.length()) {
+            return S;
+        }
+        return s;
+    }
+    
+    public static void main(String[] args) {
+        System.out.println(new CompressStringLCCI().compressString("aabcccccaa"));
+    }
+}
